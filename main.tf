ee9ff9df726a63c7c79e96fd59668b3969f0583f
kalwakolvenky <61227310+kalwakolvenky@users.noreply.github.com>
2020-07-01 14:54:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_security_group" "allow_all" {
 data "aws_ami" "my_ami" {
      most_recent      = true
      #name_regex       = "^mavrick"
-     owners           = ["721834156908"]
+     owners           = ["946995831463"]
 }
 
 
@@ -102,7 +102,7 @@ resource "aws_instance" "web-1" {
     #ami = "ami-0d857ff0f5fc4e03b"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "LaptopKey"
+    key_name = "aws"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
